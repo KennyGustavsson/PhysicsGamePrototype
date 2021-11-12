974791af950da5af507c950ff7da91d195bcfce8
Blherm <63404992+Blherm@users.noreply.github.com>
2021-11-12 10:50:52 +0100

    Updates to piston, removed unused scripts (old controller, transform follow)

--- Assets/_Game/Scripts/Piston/PistonMovement.cs
+++ Assets/_Game/Scripts/Piston/PistonMovement.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class PistonMovement : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D pistonTopRB;
-    [SerializeField] private DistanceJoint2D distanceJoint;
-    [SerializeField] private float force = 6000f;
-    [SerializeField] private float refreshRate = 2f;
-    private float timer = 0;
+    private Rigidbody2D pistonTopRB;
+    private DistanceJoint2D distanceJoint;
+    [SerializeField] private float force = 3000f;
+    [SerializeField] private float interval = 4f;
     [SerializeField] private bool startActive = false;
+    private float timer = 0;
 
     private void Awake()
     {
+        pistonTopRB = GetComponentInChildren<Rigidbody2D>();
+        distanceJoint = GetComponentInChildren<DistanceJoint2D>();
+    }
+
+    private void Start()
+    {
         if (startActive)
         {
-            timer = refreshRate / 2;
+            timer = interval / 2;
         }
     }
 
@@ -23,7 +29,7 @@ public class PistonMovement : MonoBehaviour
     {
         timer += Time.deltaTime;
 
-        if (timer >= refreshRate)
+        if (timer >= interval)
         {
             timer = 0;
             StartCoroutine(Push());            
@@ -31,7 +37,7 @@ public class PistonMovement : MonoBehaviour
     }
 
     private IEnumerator Push()
-    {
+    {        
         distanceJoint.enabled = true;
         pistonTopRB.AddForce(Vector2.up * force, ForceMode2D.Impulse);
         yield return new WaitForSeconds(1f);
