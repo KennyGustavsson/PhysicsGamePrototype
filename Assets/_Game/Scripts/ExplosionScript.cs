895ac80dc903bd37a3908d454c8012c1c59d11c2
KennyGustavsson <kenny.gustavsson93@gmail.com>
2021-11-15 10:27:39 +0100

    Fix mine explosion direction

--- Assets/_Game/Scripts/ExplosionScript.cs
+++ Assets/_Game/Scripts/ExplosionScript.cs
@@ -46,7 +46,10 @@ public class ExplosionScript : MonoBehaviour
 
 		    if (rb)
 		    {
-			    rb.AddForceAtPosition(Vector2.one * ExplosionForce, Pos); 
+			    rb.AddForceAtPosition(Vector2.one * ExplosionForce, Pos);
+
+			    Vector2 Direction = (Hit.point - Pos).normalized;
+			    rb.AddForce(Direction * ExplosionForce, ForceMode2D.Impulse); 
 		    }
 	    }
         
